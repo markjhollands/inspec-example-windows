cc167f2b0935ca30bbb21fbe6658db004f934dae
Mark Hollands <markjhollands@gmail.com>
2019-02-14 22:40:17 +0000

    Check for Windows hotfixes

--- controls/example.rb
+++ controls/example.rb
@@ -18,3 +18,19 @@ control 'WINDOWS VERSION' do
     it { should > '10.0' }
   end
 end
+
+## Looping example WannaCry Vulnerability Check
+control 'WINDOWS HOTFIX - LOOP' do
+  impact 0.8
+  title 'This test checks that a numberof Windows Hotfixs are installed - Looping Example'
+
+  hotfixes = %w{ KB4012598 KB4042895 KB4041693 }
+
+  describe.one do
+    hotfixes.each do |hotfix|
+      describe windows_hotfix(hotfix) do
+        it { should_not be_installed }
+      end
+    end
+  end
+end
